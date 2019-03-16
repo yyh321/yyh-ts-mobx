1b4561b76d19674f632a6936b0051b41b050f719
yyh123 <yunhaoyu@gmail.com>
2019-03-17 00:57:45 +0800

    init typescript-react mobx

---
+++ src/app.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { observable, action, configure } from 'mobx'
+import { observer } from 'mobx-react'
+import MobxReactDevtools  from 'mobx-react-devtools'
+
+configure({
+  enforceActions: true
+})
+
+class HelloData {
+  @observable clickedCount = 0
+
+  @action
+  increment(){
+    this.clickedCount++
+  }
+}
+
+@observer
+class Hello extends React.Component<{}> {
+  data = new HelloData();
+  render() {
+    return(
+      <button onClick={() => this.data.increment()}>
+      Click count = {this.data.clickedCount}
+      </button>
+    )
+  }
+}
+
+ReactDOM.render(
+  <>
+  <Hello />
+  <MobxReactDevtools />
+  </>,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+module.exports = {
+  entry: './src/app.tsx',
+  output: {
+    path: __dirname + '/public',
+    filename: 'build/app.js'
+  },
+  resolve: {
+    extensions: ['.ts','.tsx','.js']
+  },
+  module: {
+    rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader'
+        }
+    ]
+  }
+}
+
+
+
